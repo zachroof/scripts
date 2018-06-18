a68e3a46109fdd63e3f2697af56d7d87096ec37a
Zach Roof <zroof85@gmail.com>
2018-06-18 13:20:39 -0400

    Updating paths

--- kms-encrypt.sh
+++ kms-encrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 KMS_KEY_ID=$1 # Arn of the key
-ENCRYPT_THIS_FILE_AT_PATH=fileb://"${2:-secret}"
-PROFILE="${3:-default}"
+PROFILE="${2:-default}"
+ENCRYPT_THIS_FILE_AT_PATH=fileb://"${3:-secret}"
 
 output=$(aws kms encrypt --key-id ${KMS_KEY_ID} --plaintext ${ENCRYPT_THIS_FILE_AT_PATH} --query CiphertextBlob --output text --profile ${PROFILE})
 echo "Base64 Encoded Ciphertext Is: $output"
