1660d82f0c85eccf865a8a4c5de8e15d0d3d5bc1
Zach Roof <zroof85@gmail.com>
2018-06-18 13:19:40 -0400

    Updating for arn

--- kms-encrypt.sh
+++ kms-encrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ENCRYPT_THIS_FILE_AT_PATH=fileb://$1
-KMS_KEY_ID=$2 # Arn of the key
+KMS_KEY_ID=$1 # Arn of the key
+ENCRYPT_THIS_FILE_AT_PATH=fileb://"${2:-secret}"
 PROFILE="${3:-default}"
 
 output=$(aws kms encrypt --key-id ${KMS_KEY_ID} --plaintext ${ENCRYPT_THIS_FILE_AT_PATH} --query CiphertextBlob --output text --profile ${PROFILE})
